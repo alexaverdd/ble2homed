7184d3d036bc1477280b8de4293fd4a639ca0967
alexaverdd <152314092+alexaverdd@users.noreply.github.com>
2026-09-16 18:45:16 +0300

    Add 'steps' to exposeOrder in types.go

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -313,7 +313,7 @@ func (d *Device) GetExposeList() []DeviceExpose {
 	defer d.Mu.RUnlock()
 
 	// Фиксированный порядок полей (чтобы набор exposes не менялся местами)
-	exposeOrder := []string{"temp", "humidity", "battery", "voltage", "pressure", "illuminance"}
+	exposeOrder := []string{"temp", "humidity", "battery", "voltage", "pressure", "illuminance", "steps"}
 	
 	exposes := make([]DeviceExpose, 0, 1+len(exposeOrder))
 	
@@ -364,6 +364,11 @@ func (d *Device) GetExposeList() []DeviceExpose {
 				expose.Name = "Illuminance"
 				if expose.Unit == "" {
 					expose.Unit = "lx"
+				}
+			case "steps":
+				expose.Name = "Steps"
+				if expose.Unit == "" {
+					expose.Unit = "steps"
 				}
 			}
 
